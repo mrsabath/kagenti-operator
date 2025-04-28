d6b48417f796892a9f6fff231cf9b175fce69a3d
Jarek Cwiklik <cwiklik@mac.lan>
2025-04-28 10:34:26 -0400

    install kind with local registry
    Signed-off-by: Jarek Cwiklik <cwiklik@mac.lan>

--- beeai/scripts/install.sh
+++ beeai/scripts/install.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
 set -euo pipefail
+set -x # echo so that users can understand what is happening
+set -e # exit on error
+
+:
+: -------------------------------------------------------------------------
+: "Create Kind with local registry"
+: 
+
+curl -sL https://raw.githubusercontent.com/kagenti/kagenti-operator/refs/heads/main/scripts/kind-with-registry.sh | bash -s v0.31.0
+
+:
+: -------------------------------------------------------------------------
+: "Install Tekton Pipelines"
+: 
 
 TEKTON_VERSION="v0.66.0" 
 OPERATOR_NAMESPACE="default"
 
-echo "--- Installing Tekton Pipelines ---"
 kubectl apply --filename "https://storage.googleapis.com/tekton-releases/pipeline/previous/${TEKTON_VERSION}/release.yaml"
 
-echo "--- Installing the beeai operator ---"
+:
+: -------------------------------------------------------------------------
+: "Installing the BeeAI Operator"
+: 
+
 helm upgrade --install kagenti-beeai-operator oci://ghcr.io/kagenti/kagenti-operator/kagenti-beeai-operator-chart --version 0.0.1
-# helm upgrade --install kagenti dist/chart
 
 echo "--- Installation complete ---"
