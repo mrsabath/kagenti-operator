0a7815e5a48c52e39ff95dc74f4c073517bb6d11
Paolo Dettori <dettori@us.ibm.com>
2025-05-20 14:19:41 -0400

    fix missing command
    Signed-off-by: Paolo Dettori <dettori@us.ibm.com>

--- beeai/scripts/install.sh
+++ beeai/scripts/install.sh
@@ -3,6 +3,8 @@
 #set -euo pipefail
 set -x # echo so that users can understand what is happening
 
+TEKTON_VERSION="v0.66.0" 
+OPERATOR_NAMESPACE="kagenti-system"
 
 cluster_name="agent-platform" 
 
@@ -71,18 +73,19 @@ fi
 : -------------------------------------------------------------------------
 : "Installing Tekton Pipelines"
 : 
-
-TEKTON_VERSION="v0.66.0" 
-OPERATOR_NAMESPACE="default"
-
 kubectl apply --filename "https://storage.googleapis.com/tekton-releases/pipeline/previous/${TEKTON_VERSION}/release.yaml"
 
 :
 : -------------------------------------------------------------------------
 : "Installing the BeeAI Operator"
 : 
-
-helm upgrade --install kagenti-beeai-operator oci://ghcr.io/kagenti/kagenti-operator/kagenti-beeai-operator-chart --version 0.0.1
+LATEST_TAG=$(curl -s "https://api.github.com/repos/kagenti/kagenti-operator/tags" | \
+  jq -r '.[].name' | \
+  grep '^v' | \
+  sort -V | \
+  tail -n 1 |
+  sed 's/^v//')
+helm upgrade --install kagenti-beeai-operator --create-namespace --namespace ${OPERATOR_NAMESPACE} oci://ghcr.io/kagenti/kagenti-operator/kagenti-beeai-operator-chart --version ${LATEST_TAG}
 
 :
 : -------------------------------------------------------------------------
