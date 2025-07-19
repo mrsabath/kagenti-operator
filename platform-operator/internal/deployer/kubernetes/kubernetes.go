9b53a2f6d7bac5f0c54aabfc426e6e0d7c3caba4
Alan Cha <Alan.cha1@ibm.com>
2025-07-18 22:53:09 -0400

    Point to ghcr.io client registration image
    Signed-off-by: Alan Cha <Alan.cha1@ibm.com>

--- platform-operator/internal/deployer/kubernetes/kubernetes.go
+++ platform-operator/internal/deployer/kubernetes/kubernetes.go
@@ -235,7 +235,7 @@ func (d *KubernetesDeployer) createDeployment(ctx context.Context, component *pl
 	if d.EnableClientRegistration {
 		initContainers = append(initContainers, corev1.Container{
 			Name:            "kagenti-client-registration",
-			Image:           "kagenti-client-registration:latest",
+			Image:           "ghcr.io/kagenti/kagenti-client-registration:latest",
 			ImagePullPolicy: corev1.PullPolicy(kubeSpec.ImageSpec.ImagePullPolicy),
 			Resources:       component.Spec.Deployer.Kubernetes.Resources,
 			Env: []corev1.EnvVar{
