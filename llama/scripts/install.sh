432c5ad468960f19229930364609e422cbecdbb0
Jarek Cwiklik <cwiklik@mac.lan>
2025-09-15 12:49:28 -0400

    delete beeai,llama operators as both reached eof
    Signed-off-by: Jarek Cwiklik <cwiklik@mac.lan>

--- llama/scripts/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-echo "--- Installing the llama operator ---"
-helm upgrade --install kagenti-beeai-operator oci://ghcr.io/kagenti/kagenti-operator/kagenti-llama-operator-chart --version 0.0.1
-# helm upgrade --install kagenti dist/chart
-
-echo "--- Installation complete ---"
